ff5ba3519b63a8d1a2915cdf0c0c972aebb936f6
VvencesS <DucSoanVences99@gmail.com>
2021-02-03 23:13:50 +0700

    Calculate hashcode for data

--- src/digitalsignatures/usingjava/DigitalSignaturesUsingJava.java
+++ src/digitalsignatures/usingjava/DigitalSignaturesUsingJava.java
@@ -4,6 +4,20 @@ import java.io.*;
 import java.security.*;
 
 public class DigitalSignaturesUsingJava {
+    private static String checksum(String filepath, MessageDigest md) throws IOException {
+        // file hashing with DigestInputStream
+        try (DigestInputStream dis = new DigestInputStream(new FileInputStream(filepath), md)) {
+            while (dis.read() != -1) ; //empty loop to clear the data
+            md = dis.getMessageDigest();
+        }
+        // bytes to hex
+        StringBuilder result = new StringBuilder();
+        for (byte b : md.digest()) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+    }
+
     public static void main(String[] args) {
         /* Generate a DSA signature */
 //        if (args.length != 1) {
@@ -32,18 +46,24 @@ public class DigitalSignaturesUsingJava {
             dsa.initSign(priv);
 
             // Supply the Signature Object the Data to Be Signed
-            FileInputStream fis = new FileInputStream("data.txt");
-
-            BufferedInputStream bufin = new BufferedInputStream(fis);
-            byte[] buffer = new byte[1024];
-            int len;
-            while ((len = bufin.read(buffer)) >= 0) {
-                dsa.update(buffer, 0, len);
-            };
-            bufin.close();
+//            FileInputStream fis = new FileInputStream("data.txt");
+//
+//            BufferedInputStream bufin = new BufferedInputStream(fis);
+//            byte[] buffer = new byte[1024];
+//            int len;
+//            while ((len = bufin.read(buffer)) >= 0) {
+//                dsa.update(buffer, 0, len);
+//            };
+//            bufin.close();
+            MessageDigest md = MessageDigest.getInstance("SHA-256"); //SHA, MD2, MD5, SHA-256, SHA-384...
+            String hex = checksum("./data.txt", md);
+            dsa.update(hex.getBytes());
 
             // Generate the Signature
             byte[] realSig = dsa.sign();
+
+            //Printing the signature
+            System.out.println("Digital signature for given text: "+new String(realSig, "UTF8"));
 
             /*Save the Signature and the Public Key in Files*/
             FileOutputStream sigfos = new FileOutputStream("sig");
@@ -55,9 +75,6 @@ public class DigitalSignaturesUsingJava {
             FileOutputStream keyfos = new FileOutputStream("suepk");
             keyfos.write(key);
             keyfos.close();
-
-            /* Verifying a Digital Signature */
-
         } catch (Exception e) {
             System.err.println("Caught exception " + e.toString());
         }
--- src/digitalsignatures/usingjava/VerifyingADigitalSignature.java
+++ src/digitalsignatures/usingjava/VerifyingADigitalSignature.java
@@ -5,10 +5,22 @@ import java.security.*;
 import java.security.spec.*;
 
 public class VerifyingADigitalSignature {
+    private static String checksum(String filepath, MessageDigest md) throws IOException {
+        // file hashing with DigestInputStream
+        try (DigestInputStream dis = new DigestInputStream(new FileInputStream(filepath), md)) {
+            while (dis.read() != -1) ; //empty loop to clear the data
+            md = dis.getMessageDigest();
+        }
+        // bytes to hex
+        StringBuilder result = new StringBuilder();
+        for (byte b : md.digest()) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+    }
+
     public static void main(String[] args) {
-
         /* Verify a DSA signature */
-
         try {
             /* Input and Convert the Encoded Public Key Bytes */
             FileInputStream keyfis = new FileInputStream("suepk");
@@ -32,17 +44,20 @@ public class VerifyingADigitalSignature {
             sig.initVerify(pubKey);
 
             // Supply the Signature Object With the Data to be Verified
-            FileInputStream datafis = new FileInputStream("data.txt");
-            BufferedInputStream bufin = new BufferedInputStream(datafis);
-
-            byte[] buffer = new byte[1024];
-            int len;
-            while (bufin.available() != 0) {
-                len = bufin.read(buffer);
-                sig.update(buffer, 0, len);
-            };
-
-            bufin.close();
+//            FileInputStream datafis = new FileInputStream("data.txt");
+//            BufferedInputStream bufin = new BufferedInputStream(datafis);
+//
+//            byte[] buffer = new byte[1024];
+//            int len;
+//            while (bufin.available() != 0) {
+//                len = bufin.read(buffer);
+//                sig.update(buffer, 0, len);
+//            };
+//
+//            bufin.close();
+            MessageDigest md = MessageDigest.getInstance("SHA-256"); //SHA, MD2, MD5, SHA-256, SHA-384...
+            String hex = checksum("./data.txt", md);
+            sig.update(hex.getBytes());
 
             // Verify the Signature
             boolean verifies = sig.verify(sigToVerify);
